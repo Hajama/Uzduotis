e9ff6d7f6231bb9441ede5f4876955aa07b16171
Hajama <ASASIS1@gmail.com>
2016-10-14 16:49:38 +0300

    Add files via upload

---
+++ migrations/m161013_185940_fill_dataTables.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161013_185940_fill_dataTables extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161013_185940_fill_dataTables cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
